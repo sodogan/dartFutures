f5fb1276feac3f4d65d14b988aea28a9d6db51d0
sodogan <itelligence.i.dogan@daimler.com>
2021-05-16 21:27:25 +0300

    refactored

--- bin/utility/randomApi.dart
+++ bin/utility/randomApi.dart
@@ -5,59 +5,56 @@ import 'dart:convert';
 
 //Fetch data from the Random API
 //    {userId: 1, id: 1, title: quidem molestiae enim}
-Future<Map<String,dynamic>> fetchSingleAlbum() async{
-  final response =  await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
+Future<Map<String, dynamic>> fetchSingleAlbum() async {
+  final response =
+      await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
   if (response.statusCode == 200) {
     return jsonDecode(response.body);
-  }
-  else {
-    throw new Exception("Failed to connect to the Random API");
-  }}
-
-//Fetch data from the Random API
-//Future<List<Map<String,dynamic>>> fetchAllAlbums() async{
-Future<List<dynamic>> fetchAllAlbums() async{
-
-  final response =  await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums'));
-  if (response.statusCode == 200) {
-    var all = jsonDecode(response.body);
-    return all;
-  }
-  else {
-    throw new Exception("Failed to connect to the Random API");
-  }}
-
-
-
-Future<List<dynamic>> fetchRandomAPI() async {
-  const authority = "randomuser.me";
-  const unencodedPath = "api";
-  //call the random API
-  var response = await http.get(new Uri.https(authority, unencodedPath));
-  if (response.statusCode == 200) {
-    return jsonDecode(response.body)["results"];
-  }
-  else {
+  } else {
     throw new Exception("Failed to connect to the Random API");
   }
 }
+
+//Fetch data from the Random API
+//Future<List<Map<String,dynamic>>> fetchAllAlbums() async{
+Future<List<dynamic>> fetchAllAlbums() async {
+  final response =
+      await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums'));
+  if (response.statusCode == 200) {
+    var all = jsonDecode(response.body);
+    return all;
+  } else {
+    throw Exception("Failed to connect to the Random API");
+  }
+}
+
+Future<List<dynamic>> fetchRandomAPI() async {
+  final _apiURL = "https://randomuser.me/api/?results=50";
+  //call the random API
+  var response = await http.get(Uri.parse(_apiURL));
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body)['results'];
+  } else {
+    throw Exception("Failed to connect to the Random API");
+  }
+}
+
 //https://daimler.neptune-software.cloud/api/entity/users
 Future<List<dynamic>> fetchPlanet9API() async {
-  const authority = "daimler.neptune-software.cloud";
-  const unencodedPath = "api/entity/users";
+  const _apiURL = 'https://daimler.neptune-software.cloud/api/entity/users';
+  const _authHeader = 'Basic YWRtaW46VGFrc2ltMTI=';
 
   final response = await http.get(
-    Uri.parse('https://daimler.neptune-software.cloud/api/entity/users'),
+    Uri.parse(_apiURL),
     // Send authorization headers to the backend.
     headers: {
-      HttpHeaders.authorizationHeader: "Basic YWRtaW46VGFrc2ltMTI=",
+      HttpHeaders.authorizationHeader: _authHeader,
     },
   );
 
   if (response.statusCode == 200) {
     return jsonDecode(response.body);
-  }
-  else {
-    throw new Exception("Failed to connect to the Random API");
+  } else {
+    throw Exception("Failed to connect to the Random API");
   }
 }
